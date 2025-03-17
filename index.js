c73c2f536ae8398fdd7fc7a08b34721a9d0ad905
codeByVladimir <v_zadorko@mail.ru>
2025-03-17 22:48:02 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -9,3 +9,27 @@ openPopupBtnNode.addEventListener('click', () => {
 closePopupBtnNode.addEventListener('click', () => {
     togglePopupNode.classList.toggle('js-close');
 })
+
+//  секция репродукция / переключение контента
+
+const getBtnNodes = document.querySelectorAll('.js-France, .js-Germany, .js-England');
+const getContentNodes = document.querySelectorAll('.reproduction__list-wrapper');
+
+getBtnNodes.forEach(function(btn){
+    btn.addEventListener('click', () => {
+        const id = btn.dataset.id; 
+        getBtnNodes.forEach(function(b){
+            b.classList.remove('active');
+            
+        })
+        getContentNodes.forEach(content => {
+            content.hidden = true;
+            const contentId = document.querySelector(`.reproduction__list-wrapper[data-id="${id}"]`);
+            if(contentId){
+                contentId.hidden = false;
+            }
+        });
+        btn.classList.add('active');
+    })
+})
+
