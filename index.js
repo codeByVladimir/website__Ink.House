e808304ae7aa3f304a791826a279ceef074354bb
codeByVladimir <v_zadorko@mail.ru>
2025-03-25 19:38:49 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 const burgerOpenNode = document.querySelector('.header__burger');
 const burgerCloseNode = document.querySelector('.header__popup-close');
 const popupNode = document.querySelector('.header__popup');
+const popupNavLink = document.querySelectorAll('.header__list-link');
 
-burgerOpenNode.addEventListener('click', () => {
-    popupNode.classList.add('js-open__popup');
-});
-burgerCloseNode.addEventListener('click', () => {
-    popupNode.classList.remove('js-open__popup');
-});
+function popupIsActive(){
+    popupNode.classList.toggle('isActive');
+    document.body.classList.toggle('lock-scroll');
+};
+
+burgerOpenNode.addEventListener('click', popupIsActive);
+burgerCloseNode.addEventListener('click', popupIsActive);
+
+popupNode.addEventListener('click', (event) => {
+    if(event.target === popupNode){
+        popupIsActive();
+    }
+})
+
+popupNavLink.forEach(link => {
+    link.addEventListener('click', popupIsActive);
+})
 //  секция репродукция / переключение контента
 
 const getBtnNodes = document.querySelectorAll('.reproduction__button-nav');
